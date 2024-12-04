c1cecfce7cc703e916d87133a3b1a0c1e6a1db59
dasamaf <haltriescode@gmail.com>
2024-12-04 23:02:27 +0700

    add feature to save data to browser storage

--- script.js
+++ script.js
@@ -10,12 +10,24 @@ function addActivity(){
         listContainer.appendChild(li);
     }
     inputBox.value = "";
+    saveData()
 };
 
 listContainer.addEventListener("click", function(n){
     if(n.target.tagName === "LI"){
         n.target.classList.add("checked");
+        saveData();
     } else if(n.target.tagName === "SPAN"){
         n.target.parentElement.remove();
+        saveData();
     }
 });
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+};
+function showActivity(){
+    listContainer.innerHTML = localStorage.getItem("data");
+};
+
+showActivity();
